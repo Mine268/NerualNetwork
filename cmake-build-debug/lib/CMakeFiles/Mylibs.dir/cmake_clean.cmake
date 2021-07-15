9e7d9753f6d952f76d1976f82903f726aedb301f
lineng <1925466036@qq.com>
2021-07-15 16:03:55 +0800

    My-version

---
+++ cmake-build-debug/lib/CMakeFiles/Mylibs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mylibs.dir/Layer.cpp.obj"
+  "libMylibs.a"
+  "libMylibs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mylibs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
