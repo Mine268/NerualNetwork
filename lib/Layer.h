2694f0d617926ed16005bc9b02e8ab896dc2534c
God <57819926+Godones@users.noreply.github.com>
2021-07-18 10:44:18 +0800

    修改一些名称加入一点说明

--- lib/Layer.h
+++ lib/Layer.h
@@ -17,43 +17,42 @@ private:
     int output_size;
     node_type *biases;
     node_type *value; //a = f(z)
-    node_type *integration;//z =w x+b
+    node_type *integeration;//z =w x+b
     node_type **weight;
+
     node_type *ddelta;//
-    node_type *dbiases;
-    node_type **dweight;
-    node_type *inputdata;
+    node_type *dbiases;//最终版本应该会删除，此处只用于测试层是否工作正常
+    node_type **dweight;//也会被删除
+    node_type *preval;//前一层的激活值
 
 
-    IFunction &actfunction;
+    IFunction &activation;
     void init_wb();//初始化weight与biases
     void new_data();//申请空间
 
 public:
 
-    Layer(int _inputsize, int _outputsize, IFunction& function);
+    Layer(int _inputsize, int _outputsize, IFunction& function);//初始化层
 
-    Layer(const Layer&);
+    Layer(const Layer&);//复制构造函数
 
-    ~Layer();
+    ~Layer();//销毁申请的空间
 
-    void set_input(node_type *input);//传入上一层的value
+    void set_input(const node_type *input);//传入数据
 
-    void read_output() const;
+    const node_type *read_output() const;//返回激活值
 
-    void forward(bool useactfunction = true);
+    void forward_propagation(bool use_activation = true);//前向传播
 
-    void backward(Layer &next);
+    void back_propagation(Layer &preLayer);//反向传播
 
-    node_type *get_val();
-
-    IFunction& get_IFunction();
+    IFunction& get_IFunction();//获取激活函数
 
     void set_ddelta(node_type *delta);  //传入下一层的ddelta
 
-    friend ostream &operator<<(ostream &os, Layer &one);
+    friend ostream &operator<<(ostream &os, Layer &one); //打印层的相关信息
 
-    void dubug();
+    void debug();
 
     void debug2();
 };
--- src/Layertest.cpp
+++ src/Layertest.cpp
@@ -9,24 +9,24 @@ void test_forward_backward(){
     Layer layer(3,2,sigmoid);
     cout<<layer;
     layer.set_input(x);//输入
-    layer.forward();//前向传播计算
+    layer.forward_propagation();//前向传播计算
     layer.read_output();//打印value
-    layer.dubug();//打印integeration
+    layer.debug();//打印integeration
 
 
     Layer layer1(2,1,sigmoid);
     cout<<layer1;
-    layer1.set_input(layer.get_val());//输入
-    layer1.forward(false);//前向传播计算,到输出层不需要使用激活函数
+    layer1.set_input(layer.read_output());//输入
+    layer1.forward_propagation(false);//前向传播计算,到输出层不需要使用激活函数
     layer1.read_output();//打印value
-    layer1.dubug();//打印integeration
+    layer1.debug();//打印integeration
 
     //反向传播测试
     double ddelta[] = {-0.22};
     layer1.set_ddelta(ddelta);
     layer1.debug2();
 //
-    layer.backward(layer1);
+    layer.back_propagation(layer1);
     layer.debug2();
 }
 void test_copy_operator(){
