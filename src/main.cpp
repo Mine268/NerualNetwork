f757696e9cac1a81b201e64765aeebf09f444b6f
mine268 <mine268@foxmail.com>
2021-08-03 16:45:03 +0800

    Network类的evaluate方法完成了

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include "../lib/Network.h"
 
 int main() {
-	Network nn({Network::layer((std::size_t)10, func_sigmoid),
-				Network::layer((std::size_t)12, func_sigmoid),
-				Network::layer((std::size_t)10, func_sigmoid)});
+	Network nn({Network::layer(10, func_sigmoid),
+				Network::layer(12, func_sigmoid),
+				Network::layer(10, func_sigmoid)});
 	return 0;
 }
