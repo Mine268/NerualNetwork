1256e53564e30256d34861aa6865f74986e46091
mine268 <mine268@foxmail.com>
2021-08-04 13:51:24 +0800

    Network::for_propagation()完成

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,18 @@
 #include "../lib/Network.h"
 
 int main() {
-	Network nn({Network::layer(10, func_sigmoid),
-				Network::layer(12, func_sigmoid),
-				Network::layer(10, func_sigmoid)});
+	Network nn({Network::layer(2, func_sigmoid),
+				Network::layer(3, func_sigmoid),
+				Network::layer(3, func_sigmoid)});
+
+    nn.print_network(std::cout);
+
+    auto res = nn.evaluate(new double[2]{1., -1.});
+
+    for (std::size_t i = 0; i < 3; ++i)
+        std::cout << res[i] << ' ';
+
+    std::cout << std::endl;
+
 	return 0;
 }
